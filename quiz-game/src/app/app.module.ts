4adaa5a82bf3124960891cdeae0a3ffa8b9d7905
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2022-09-21 17:14:07 +0430

    angular material added and separate modul created

--- quiz-game/src/app/app.module.ts
+++ quiz-game/src/app/app.module.ts
@@ -5,18 +5,13 @@ import { AppComponent } from './app.component';
 import { HintsComponent } from './hints/hints.component';
 import { StagesComponent } from './stages/stages.component';
 import { MainComponent } from './main/main.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/material.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HintsComponent,
-    StagesComponent,
-    MainComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HintsComponent, StagesComponent, MainComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
