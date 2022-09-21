78840b8d391d05e642e4710cbd205ea6ff3b333f
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2022-09-21 17:53:42 +0430

    basic material components

--- quiz-game/src/app/shared/material.module.ts
+++ quiz-game/src/app/shared/material.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatButtonModule],
+  exports: [MatButtonModule, MatIconModule, MatCardModule, MatListModule],
 })
 export class MaterialModule {}
--- quiz-game/src/app/stages/stages.component.ts
+++ quiz-game/src/app/stages/stages.component.ts
@@ -3,13 +3,35 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-stages',
   templateUrl: './stages.component.html',
-  styleUrls: ['./stages.component.scss']
+  styleUrls: ['./stages.component.scss'],
 })
 export class StagesComponent implements OnInit {
+  gameStages = {
+    1: 100,
+    2: 200,
+    3: 300,
+    4: 400,
+    5: 500,
+    6: 1000,
+    7: 1500,
+    8: 2000,
+    9: 3000,
+    10: 5000,
+    11: 10000,
+    12: 20000,
+    13: 30000,
+    14: 50000,
+    15: 100000,
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  stageWinningsArray(): any {
+    // console.log(Object.entries(this.gameStages));
+    return Object.values(this.gameStages).reverse();
+
+    return [];
   }
-
 }
