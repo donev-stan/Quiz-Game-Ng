83a9e0747127e35dc26352c8c82ba3a9764df75e
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2022-09-21 17:06:55 +0430

    basic components and structure

--- quiz-game/src/app/app.module.ts
+++ quiz-game/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HintsComponent } from './hints/hints.component';
+import { StagesComponent } from './stages/stages.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HintsComponent,
+    StagesComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
