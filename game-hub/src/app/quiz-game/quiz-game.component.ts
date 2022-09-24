0355eed314857bc5424443eb14fdf356a6c1e74b
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2022-09-24 22:32:37 +0300

    app name changed

--- game-hub/src/app/quiz-game/quiz-game.component.ts
+++ game-hub/src/app/quiz-game/quiz-game.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-quiz-game',
   templateUrl: './quiz-game.component.html',
-  styleUrls: ['./quiz-game.component.scss']
+  styleUrls: ['./quiz-game.component.scss'],
 })
 export class QuizGameComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
